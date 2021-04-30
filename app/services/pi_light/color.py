998fb323e2fd9448135191550c6241706ca46dd7
Peter Gebhard <pgebhard@gmail.com>
2021-04-30 00:24:31 -0500

    Change color defaults and add color hex conversion

--- app/services/pi_light/color.py
+++ app/services/pi_light/color.py
@@ -2,10 +2,16 @@ from pydantic import BaseModel, Field
 
 
 class Color(BaseModel):
-    r: int = Field(255, ge=0, le=255)
-    g: int = Field(255, ge=0, le=255)
-    b: int = Field(255, ge=0, le=255)
-    brightness: float = Field(1, ge=0, le=1)
+    r: int = Field(0, ge=0, le=255)
+    g: int = Field(0, ge=0, le=255)
+    b: int = Field(0, ge=0, le=255)
+    brightness: float = Field(0, ge=0, le=1)
+
+    @staticmethod
+    def from_hex(hex_str: str, brightness: float = 0.0):
+        h = hex_str.lstrip('#')
+        rgb = tuple(int(h[i:i + 2], 16) for i in (0, 2, 4))
+        return Color(r=rgb[0], g=rgb[1], b=rgb[2], brightness=brightness)
 
     @staticmethod
     def gradient(c1, c2, percentage: float):
--- tests/test_services/pi_light/test_color.py
+++ tests/test_services/pi_light/test_color.py
@@ -4,6 +4,13 @@ from app.services.pi_light.color import Color
 
 
 class TestColor(TestCase):
+    def test_from_hex(self) -> None:
+        expected_color1 = Color(r=35, g=169, b=221, brightness=0.5)
+        expected_color2 = Color(r=35, g=169, b=221)
+
+        self.assertEqual(expected_color1, Color.from_hex("#23a9dd", 0.5))
+        self.assertEqual(expected_color2, Color.from_hex("#23a9dd"))
+
     def test_gradient(self) -> None:
         color1 = Color(r=1, g=2, b=4, brightness=0.5)
         color2 = Color(r=5, g=6, b=10, brightness=1)
